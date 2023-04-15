c8f911e929f592ef954032827033aa455b0fb2d7
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-04-14 23:36:38 -0300

    aula 7

---
+++ seção/aula7/intdex.js
@@ -0,0 +1,7 @@
+//JavaScript é um linguagem dinâmica 
+console.log('Hello Word'); //String 
+console.log("Hello Word");
+console.log('Luiz Otávio');
+
+console.log(123456, 15.85, );
+console.log(35, 15.85, "Luiz Otávio de Novo");
