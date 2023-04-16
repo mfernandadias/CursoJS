1be6617ebb252e13b0366a15f6ba2a98fe8900da
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-04-16 18:04:53 -0300

    aula13
    variáveis com let

---
+++ seção/aula13/index.js
@@ -0,0 +1,33 @@
+/*let nome = 'Henrique'; //String
+ 
+console.log(nome, 'nasceu em 1984');
+console.log("em 2000", nome, "conheceu Maria");
+console.log(nome, "casou-se com Maria em 2012");
+console.log("O filho de", nome, "se chama Eduardo");
+*/
+
+/*let nome; //declarou a variável
+nome = "João"; //inicializando a variável 
+console.log(nome);
+nome = "Otávio";
+console.log(nome);
+*/
+//não podemos criar cariáveis com palavras reservadas 
+//variáveis precisam ter nomes significativos 
+
+let nomeCliente = 'João';
+console.log(nomeCliente);
+
+//não pode começar o nome de uma variável com um número 
+//não podem conter espaços ou traços; 
+//utilizamos camelCase 
+
+let nomeCompletoDoCliente = 'Luiz Otávio';
+
+//case-sensitive
+let nomeFernandaCliente = 'Luiz';
+let nomeMariaCliente = 'Otávio';
+//não podemos redeclarar cariáveis com let 
+//NÃO UTILIZE VAR, UTILIZA LET
+
+
