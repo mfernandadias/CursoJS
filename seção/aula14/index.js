20c295718442ac053141a751627bbb0278ffe4a2
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-04-16 18:29:19 -0300

    aula14

---
+++ seção/aula14/index.js
@@ -0,0 +1,27 @@
+//Não podemos criar cariáveis com palavras reservadas 
+//variáveis presicam ter nomes  significativos 
+//não pode começar o nome de uma variável com um número 
+//não podem conter espaços ou traços 
+//utilizamos camelCase 
+//Case-sentive 
+//não podemos redeclarar variáveis com let 
+//não utilize var, utilize let 
+//não pode modiicar o valor de uma constante 
+
+// + - * /
+//String = Text | Number = número 
+/*JavaScript também é uma linguagem de tipagem fraca, 
+o que significa que permite a conversão implícita de tipo quando uma 
+operação envolve tipos incompatíveis, em vez de gerar erros de tipo.*/ 
+const primeiroNumero = '5';
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2; 
+let resultadoTriplicado = resultado * 3; 
+resultadoTriplicado = resultadoTriplicado + 5; 
+
+console.log(typeof(primeiroNumero + segundoNumero));
+
+
+
+
