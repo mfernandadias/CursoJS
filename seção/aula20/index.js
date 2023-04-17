3c7becdb8107df81c56d36dabd91c3f378810c1c
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-04-17 20:23:43 -0300

    aula20
    operadores aritméticas, de atribuição e incremento

---
+++ seção/aula20/index.js
@@ -0,0 +1,51 @@
+//operadores aritméticos, de atribuição e incremento 
+
+
+/*Aritméticos + - / * 
++  adição / contenação ( - / *)
+- / * 
+% resto da divisão 
+*/
+/*
+const num1 = 5; 
+const num2 = 2;
+const num3 = 10;
+console.log(num1 * num2 / num3);
+*/ 
+//incremento = ++
+// Decremento = --
+
+/*let contador = 1; 
+contador++; //2
+contador++; //3
+contador++; //4
+contador++; //5
+contador++; //6
+console.log(contador);
+*/
+
+//let contador = 10;
+//console.log(--contador);
+//console.log(--contador);
+
+//operadores de atribuição 
+/*
+let contador = 2; 
+contador *= 2; 
+contador *= 2;
+contador *= 2;
+console.log(contador);
+*/
+
+//operadores de potencialização
+/*let contador =2; 
+contador **= 10; 
+console.log(contador);
+*/ 
+
+//NaN - Not a number parseInt (inteiro), parseFloat(decimais)
+const num1 = 10; 
+const num2 = Number('5.2');
+console.log(num1 * num2);
+console.log(typeof num2);
+
