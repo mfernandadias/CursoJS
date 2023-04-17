18f777436703ad1b78d84ebc0fb4b211d30ad393
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-04-17 12:35:55 -0300

    aula19

---
+++ seção/aula19/index.js
@@ -0,0 +1,19 @@
+//tipos de dados primitivos 
+//String, number, undefined, boolean, symbol
+ 
+const nome = 'Luiz'; //String
+const nome1 = "luiz"; //String
+const nome2 = `Luiz`; //String
+
+const num1 = 10; //number 
+const num2 = 10.52; //number
+let nomeAluno; //undefined = não aponta pra local nenhuma na memória 
+let sobrenomeAluno = null; //nulo -> não aponta pra local nenhuma na memória 
+const boolean = true //Boolen = true, false (lógico) 
+
+let a = 2;
+const b = a;
+console.log(a, b); ///2, 2
+
+a = 3;
+console.log(a, b); //3, 2
