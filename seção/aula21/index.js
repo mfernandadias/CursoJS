0678046f3274c17e5951d18678d5e25f016583d6
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-04-17 20:55:09 -0300

    aula21
    alert, confirm e prompt

---
+++ seção/aula21/index.js
@@ -0,0 +1,13 @@
+//alert, confirm e pronpt(navegador)
+
+
+//função é diferente de método 
+
+const num1 = prompt ('Digite um número');
+const num2 = prompt ('Digite um número');
+/* 
+digite um número 
+digite outro número 
+faço uma conta de adição
+*/
+
