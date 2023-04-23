302b1f048be6180fef14df4d35fb1817423f0474
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-04-22 23:01:21 -0300

    aula26

---
+++ seção/index.js
@@ -0,0 +1,41 @@
+/*let num1 = 0.7; //number
+let num2 = 0.1; //number
+
+num1 += num2; //0.8
+num1 += num2; //0.9 
+num1 += num2; //1.0
+num1 += num2; //1.1
+num1 += num2; //1.2
+num1 += num2; //1.3
+num1 += num2; //1.4
+num1 += num2; //1.5
+
+num1 = Number(num1.toFixed(2));
+
+console.log(num1);
+console.log(Number.isInteger(num1)); */
+
+//console.log(num1.toString() + num2);
+//num1 = num1.toString(); 
+//console.log(typeof num1);
+//console.log(num1.toString(2)); versão binaria do número 
+//console.log(num1.toFixed(4));
+//console.log(Number.isTnterger(num1)); verdade ou falto
+/*let temp = num1 * 'Olá';
+console.log(Number.isNaN(temp)); */
+
+
+
+
+
+
+
+//IEEE 7544-2088
+let num1 =0.7; //number 
+let num2 = 0.1; //number 
+num1 = (num1 * 100) + (num2 * 100); //0.8 
+console.log(num1);
+console.log(NUmber.isInteger(num1));
+
+
+num1 = Number(num1.toFixed(2));
