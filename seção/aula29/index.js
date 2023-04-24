e3402e0de726ebb9ff60b10205bb973eabf71ed9
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-04-24 14:59:46 -0300

    aula29
    arrays (Básico) um lista de coisas

---
+++ seção/aula29/index.js
@@ -0,0 +1,43 @@
+//arrays (Básico) um lista de coisas 
+           // 012345678910
+//const nome = 'Luiz Otávio';
+//console.log(nome[0]);
+
+//               0       1        2  
+let alunos = ['Luiz', 'Maria', 'João'];
+alunos = 123; 
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+
+//alunos.push('Luiza');
+//alunos.push('Eduardo');
+
+//console.log(alunos.slice(0, -2));
+
+/*delete alunos[1]; //excluir alguem na lista
+console.log(alunos);
+console.log(alunos[50]);*/
+
+/*console.log(alunos[0]);
+console.log(alunos[2]);*/
+
+//alunos[0] = 'Eduardo';
+//alunos[3] = 'Luiza';
+/*console.log(alunos.length);  //tamanho do meu array
+alunos[alunos.length] = 'Luiza'; //adiciona no fim
+alunos[alunos.length] = 'Fábio';
+*/
+
+//alunos.push('Otávio'); //adiciona no começo
+//alunos.unshitft('Luiza'); /// adicona no começo
+//alunos.unshift('Fábio');
+/*const removido = alunos.pop(); //retiro o ultimo
+console.log(removido);
+console.log(alunos);
+*/
+
+
+
+
