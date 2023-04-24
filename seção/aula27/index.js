1f7d40fe34e1723088b88ee4f6aa9fdd270c47f6
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-04-23 22:18:25 -0300

    aula26.1

---
+++ seção/aula27/index.js
@@ -0,0 +1,24 @@
+/*let num1 = 9.44578; 
+//let num2 = Math.flor(num1);
+//let num2 = Math.celi(num1);
+
+let num2 = Math.round(num1);
+console.log(num2); */
+
+//let num1 = 9.49; 
+//console.log(Math.max(1,2,3,4,5, -10, -50, 1500, 9, 8,7,6));
+//console.log(Math.min(1,2,3,4,5, -10, -50, 1500, 9, 8,7,6);
+
+/*let num1 = 9.49; 
+const aleatorio = Math.random() * (10 - 5) + 5;
+console.log(aleatorio);
+*/ 
+
+/*let num1 = 9.49; 
+console.log(Math.pow());
+*/ 
+//raiz quadrada 
+/*let num1 = 25; 
+console.log(num1 ** 0.5); */ 
+
+console.log(100 / 0 ); //infinity 
