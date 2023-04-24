8f84eb8910dc5dda64b7549c2ea6d6d608c58b0a
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-04-24 15:43:15 -0300

    aula31
    funções básicas

---
+++ seção/aula31/index.js
@@ -0,0 +1,38 @@
+//funções básicas
+//funções executam ações 
+//função recebe um valor, pode executar ou não uma ação, 
+//e pode  ou não retornar um valor 
+
+/*function saudacao(nome){
+    return `Bom dia ${nome}!`;
+}
+
+const variavel = saudacao('Luiz');
+console.log(variavel); */
+
+//função não necessáriamente precisa de ponto e 
+//vírgula no final 
+/*function soma(x = 1, y = 1) {
+    const resultado = x + y;
+    return resultado; 
+}
+const resultado = soma(2, 2);
+console.log(resultado);*/
+
+/*console.log(soma(2,2));
+console.log(soma(3, 1));
+console.log(soma(5, 10)); */
+
+//jogar uma funçao dentro uma variável 
+//função 
+const raiz = n => n ** 0.5;
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+
+
+
+
+
+
