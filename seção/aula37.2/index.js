fd6a4f8772e3f996c3baa3695cdb006fc3e6f1a1
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-04-26 18:00:09 -0300

    aula37.2

---
+++ seção/aula37.2/index.js
@@ -0,0 +1,23 @@
+//operadores de comparação 
+/*
+> maior que 
+>= maior que ou igual a 
+< menor que 
+<= menor que ou igual a 
+== igualdade (valor) ******* 
+=== igualdade estrita (valor e tipo) 
+!= diferente (valor) ***********
+!==   diferente estrito (valor e tipo)
+*/
+
+//const comp = 10 > 5; 
+//console.log(comp); 
+
+//dedução 
+/*const num1 = 10; //number
+const num2 = '10'; //string
+const comp = num1 == num2; 
+console.log(comp); */
+
+
+
