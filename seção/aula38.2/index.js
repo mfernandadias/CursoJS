310829d4a5aeeff2fb043b703ca3c94d5668b47a
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-04-30 17:04:04 -0300

    continuação da aula 38

---
+++ seção/aula38.2/index.js
@@ -0,0 +1,19 @@
+/* 
+Operadores Lógicos 
+&& -> AND -> E 
+|| -> OR -> OU 
+! -> NOT -> NÃO
+*/ 
+
+//consolo.log();
+//const expressaoOr = false || false; 
+//console.log(expressaoOr); 
+
+/*const usuario = 'Luiz';
+const senha = '123456';
+
+const vaiLogar = usuario === 'Luiz' && senha === '12345'; 
+console.log(vaiLogar); */
+
+//console.log(!false);
+
