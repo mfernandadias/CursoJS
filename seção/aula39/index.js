c4e7a4bbcc3500a8275ee52447affd267bc23bbf
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-04-30 17:26:06 -0300

    aula39

---
+++ seção/aula39/index.js
@@ -0,0 +1,47 @@
+/*
+&& -> 
+|| ->
+FALSY 
+*false 
+0 
+' ' " " ``
+null / undefined 
+NaN 
+*/
+
+//console.log('Luiz' && ' ' && 'Maria');
+
+/*function falaOi () {
+    return 'Oi';
+}
+
+const vaiExecutar = false; 
+
+vaiexecutar && console.log(falaOi()); */
+
+/*function falaOi () {
+    return 'Oi';
+}
+
+const vaiExecutar = 'Joãozinho';
+console.log(vaiExecutar && falaOi()); */
+
+//console.log(0 || false || null || 'Luiz Otávio' || true);
+
+/*const corUsusario = 'vermelho'; 
+const corPadrao = corUsuario || 'preto';
+
+console.log(corPadrao);*/
+
+const a = 0; 
+const b = null; 
+const c = 'false';  //aqui, String, pq esta dentro de aspas por isso tem valor 
+const d = false;
+const e = NaN; 
+
+console.log(a || b || c || d || e);
+
+
+
+
+
