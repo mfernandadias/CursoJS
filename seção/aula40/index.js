b4120f8d6006618699dbfa90ced6c9258f670b2d
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-04-30 18:16:13 -0300

    aula40

---
+++ seção/aula40/index.js
@@ -0,0 +1,28 @@
+//if, else, ife else (1)
+
+// if pode ser usado sozinhoa 
+//else if não pode ser executado sozinho 
+//sempre que utilizo a palavra else, preciso de um if antes 
+// eu posso ter quantos else ifs na checagem 
+
+/*
+exemplo 1: 
+const hora = 50; 
+if (hora >= 0 && hora <= 11) {
+    console.log('Bom dia');
+} else if(hora >= 12 && hora <= 17) {
+    console.log('Boa tarde');
+} else if ( hora >= 18 && hora <= 23) {
+    console.log('Boa noite');
+} else {
+    console.log('Olá');
+}
+*/
+
+//exemplo 2: 
+const tenhoGrana = 'Luis'; 
+if (tenhoGrana) {
+    console.log('Vou sair de casa');
+} else {
+    console.log('Não vou sair de casa');
+}
