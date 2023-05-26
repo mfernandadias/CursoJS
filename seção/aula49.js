2fbe78a8120a9a260e841b9ac5bcaccf88023e35
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-05-26 01:10:17 -0300

    aula49
    mais diferenças entre var e let/const

---
+++ seção/aula49.js
@@ -0,0 +1,49 @@
+/*const verdadeira = true;
+
+//let tem escopo de bloco { ... bloco }
+// var só tem escopo de função
+
+let nome = 'Luiz'; //criando
+var nome2 = 'Luiz';
+
+if (verdadeira) {
+    let nome = 'Otávio'; //criando
+    //console.log(nome, nome2);
+
+    if(verdadeira) {
+        let nome = 'Outra coisa';
+        console.log(nome, nome2);
+    }
+} */
+
+
+/*
+let nome = 'Luiz'; //criando
+var nome2 = 'Luiz'; // criando
+
+if (verdadeira) {
+    let nome = 'Otávio'; //criando
+    var nome2 = 'Rogério'; //redeclarando
+
+    if (verdadeira) {
+    var nome2 = 'Ronado'; //redeclarando
+    let nome  = 'Outra coisa';
+  }
+}
+
+console.log(nome, nome2); */
+
+/*var sobrenome = 'Miranda';
+
+function falaOi () {
+    console.log(sobrenome);
+}
+falaOi(); */
+
+var sobrenome;
+const verdadeira = true; 
+
+sobrenome = 'Miranda';
+
+console.log(sobrenome);
+
