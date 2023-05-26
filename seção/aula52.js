ea1f2c4be3d8e4523579de35236223bce92bedfb
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-05-26 19:07:50 -0300

    aula52.js
    for - clássico: estrutura de repetição

---
+++ seção/aula52.js
@@ -0,0 +1,21 @@
+//for - clássico: estrutura de repetição 
+ //criar váriavel de controle
+ //
+
+ //i - index
+ //controla for pela condição 
+/*for (let i = 0; i <= 6; i++) {
+    console.log(`Linha ${i}`);
+ }
+ */
+
+ /*for (let i = 0; i <= 10; i++) {
+    const par = i % 2 === 0 ? 'par' : 'impar';
+    console.log(i, par);
+ } */
+
+ //
+ const frutas = ['Maça', 'Pêra', 'Uva']; console.log(frutas)
+ for (let i =0; i < frutas.length; i++) {
+    console.log(`Indice ${i}`,frutas[i]);
+ }
