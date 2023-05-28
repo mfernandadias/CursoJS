a7bdac8f4597c3d6c072075eab11eb5798495f87
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-05-28 16:42:06 -0300

    aula57
    exercícios com NodeList (brower)

---
+++ seção/aula57/aula57.js
@@ -0,0 +1,13 @@
+//exercícios com NodeList(brower)
+const paragrafos = document.querySelector('.paragrafos');
+const ps = paragrafos.querySelectorAll('p');
+
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorBody = estilosBody.backgroundColor; 
+console.log(backgroundColorBody);
+
+for (let p of ps) {
+    console.log(p);
+    p.stylebackgroundColor = backgroundColorBody; 
+    p.style.color = '#FFFFFF';
+}
