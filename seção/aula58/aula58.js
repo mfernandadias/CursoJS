6a746809d7ff77942a75e9885f1f4959ad475d5d
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-05-28 17:58:40 -0300

    aula 58
    while e do while

---
+++ seção/aula58/aula58.js
@@ -0,0 +1,31 @@
+//while e do while - estrutura de repetição 
+
+/*let i = 0; 
+
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
+console.log('Segue a vida...'); */
+
+function random(min, max) {
+    const r = Math.random() * (max - min) + min; 
+    return Math.floor(r);
+}
+
+const min = 1; 
+const max = 50; 
+let rand = 10;
+
+while(rand !== 10) {
+    rand = random(min, max);
+    console.log(rand);
+}
+
+console.log('######');
+
+do { 
+    rand = random(min, max);
+    console.log(rand);
+}   while(rand !== 10);
