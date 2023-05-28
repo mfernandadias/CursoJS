dfc81e8473fad5a1a03ca5ba9674aac8e3edf34e
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-05-28 19:15:56 -0300

    aula 60
    exercício com lógica de programação

---
+++ seção/aula60/aula60.js
@@ -0,0 +1,12 @@
+//escreva uma função que receba 2 números e retorne o maior deles 
+/*function max(x, y) {
+    if (x > y) {
+        return x;
+    }
+    return y;
+}
+
+console.log(max(10, 2)); */
+
+const max2 = (x, y) => x > y ? x : y;
+console.log(max2(10, 20));
