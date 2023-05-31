90503545342ea026d0a382829be5da86eb1eb0c0
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-05-31 17:03:23 -0300

    aula 66
    parte2

--- seção/aula66/aula66.js
+++ seção/aula66/aula66.js
@@ -1,1 +1,50 @@
 //criando um timer com setInterval
+
+//new Date(0);
+function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false, 
+        timeZone: 'UTC'
+    });
+}
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+let segundos = 0; 
+let timer;
+
+function inciarRelogio() {
+    const timer = setInterval(function() {
+        segundos++;
+        relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000)
+}
+
+<document.addEventListener('click', function(e) {
+    console.log(e.target);
+
+    if(el.classList.contains('zerar') {
+        clearInterval(timer);
+        relogio.ineerHTML = '00:00:00';
+        segundos = 0;
+    } 
+
+    if(el.classList.contains('zerar') {
+        
+    }
+
+
+});
+
+iniciar.addEventListener('click', function(event){
+});
+
+pausar.addEventListener('click', function(event){
+    clearInterval(timer);
+});
+
+zerar.addEventListener('click', function(event){
+    
+});
