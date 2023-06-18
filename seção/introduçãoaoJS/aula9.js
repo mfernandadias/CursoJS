5218b160fd9bf366eaf320e48c78cd030b413f7b
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-06-18 19:59:00 -0300

    parte9
    revisão number

---
+++ seção/introduçãoaoJS/aula9.js
@@ -0,0 +1,35 @@
+//number 
+//São todos os números reais que aprendemos nas aulas de matemátrica 
+
+let numeroInteiro = 100;
+let numeroDecimal = 5.38; 
+let numeroNegativo = -200.99;
+
+//operadores 
+//você pode usar +, -, * e / para executar 
+//as operações básicas com números 
+
+let numero = 5; 
+let numero2 = 7; 
+let resultado = numero + numeroo2; 
+
+let numero3 = 5; 
+let resultado2 = numero * 0.5;
+
+let numero4 = 5; 
+//cuidado: 
+//dividão por 0 causará erro 
+let resultado3 = numero / 0;
+
+//importante 
+/*
+estes valores numéricos não são infinitos. Mesmo assim, o JavaScript 
+trabalha com ponto flutuante de dupla precisão (double) que permite valores 
+muitos altos. 
+Máximo e Mínimo: 
+para saber quais são estes valores máximos e mínimos, você pode efetuar os seguintes 
+comandos no console. 
+*/
+console.log(Number.MAX_VALUE);
+
+console.log(Number.MIN_VALUE);
