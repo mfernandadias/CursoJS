9229fe26da30700933b5f1589597501d1ccd1e3e
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-06-18 20:30:56 -0300

    revisão booleanos

---
+++ seção/introduçãoaoJS/aula11.js
@@ -0,0 +1,29 @@
+//booleanos 
+
+/*
+São tipos de dados que possuem apenas dois valores: 
+TRUE: Verdadeiro
+False: falso
+*/
+
+let verdadeiro = true; 
+let falso = false; 
+
+/*
+observações:
+os valores true e dalse NÃo são escrtitos entre aspas. 
+cuidade! O JavaScript entende certos valores dos outros tipos de 
+dados como true ou false.
+
+Comparações: uma das formas mais comuns de se obter valores booleanos é 
+através de comparações.
+*/
+
+//true 
+let comparacao = 1 == 1; 
+
+//false 
+let comparacao2 = 1 > 5;
+
+//true 
+let comparacaoString = "banana" == "banana";
