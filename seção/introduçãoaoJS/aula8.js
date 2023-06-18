25b357b1a2f852a741d04a0492efab098c8c59bb
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-06-18 18:35:25 -0300

    parte 8
    tipos de dados

---
+++ seção/introduçãoaoJS/aula8.js
@@ -0,0 +1,14 @@
+//tipod de dados 
+//o JavaScript possui os seguintes tipos de dados: 
+//number --> null e Undefined 
+//String --> Symbol* 
+//Boolean --> object 
+//arrya --> function 
+
+//importância 
+/*
+saber este tipo de informação é muito importante, pois ajuda a
+identfificar erros. 
+Mais a frente, você verá estrutura que recebem parâmetros e se informarmos  
+o tipo errado, o programa poderá não funcionar como deveria.
+*/
