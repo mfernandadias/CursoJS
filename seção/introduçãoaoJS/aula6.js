86c4febab82565c4c2a72a9fa42c57a642892a18
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-06-18 18:05:26 -0300

    parte7
    o que são variáveis

---
+++ seção/introduçãoaoJS/aula6.js
@@ -0,0 +1,22 @@
+//introdução 
+let numero = 120; 
+
+var texto = "String";
+
+const booleano = true; 
+
+let vetor = [1, 2, 3, 4, 5, 6, 7, 8];
+
+let objeto = { 
+    propriedade: 12,
+    propriedade2: "teste"
+};
+
+let umaFuncao = function () {
+    //Fazer algo
+};
+
+function outraFuncao() {
+    umaFuncao();
+    umaFuncao();
+}
