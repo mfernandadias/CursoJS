4e767a47b70076a4e8b4655f72d1982819d97956
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-03 20:59:41 -0300

    função de callback

--- seção/aula73.js
+++ seção/aula73.js
@@ -1,1 +1,41 @@
-function f1() {}
+function rand(min = 1000, max = 3000){
+    const num = Math.random() * (max - min) +  
+    min; 
+    return Math.floor(num);
+}
+
+console.log(rand());
+
+function f1(callback) {
+    setTimeout(function(){
+     console.log('f1');  
+    }, 500);
+}
+
+function f2(callback) {
+    setTimeout(function() {
+        console.log('f2');
+    }, 1000);
+}
+
+function f3(callback) {
+    setTimeout(function(){
+        console.log('f3');
+        if(callback) callback();
+    }, rand()); 
+}
+
+f1(f1Callback);
+
+function f1Callback(){
+    f2(f2Callback);
+}
+
+function f2Callback(){
+    f3(f3Callback);
+}
+
+function f3callback() {
+    console.log('Olá Mundo!');
+}
+
