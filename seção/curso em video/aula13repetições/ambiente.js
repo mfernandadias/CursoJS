1d56fee9d9f6c0d1bc954662a55de9aa2e506bfd
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-14 21:10:15 -0300

    repetições.14

--- seção/curso em video/aula13repetições/ambiente.js
+++ seção/curso em video/aula13repetições/ambiente.js
@@ -4,3 +4,17 @@ while (c < 500) {
     console.log('Tudo bem?')
     c++
 }
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+/*
+já aprendeu a usar direito a extensão watch in chrome no VSCode ?
+já aprendeu a usar o Modo de Depuração do VSCode ? 
+Já fez todos os testes com as estruturas do while, do..while e for ? 
+que aprender um pouco mais manipular dados em controle HTML com JS? 
+*/
---
+++ seção/curso em video/aula14.repetições/ambiente.js
@@ -0,0 +1,21 @@
+/* for (var c=1; c<=10; c++){
+
+}
+*/
+/*
+var c = 1
+do {
+    console.log(c)
+    c++
+}
+*/
+
+/*for(var c = 1; c <= 5; c++) {
+    console.log(c)
+} */
+
+console.log('Vai começar...')
+for(var c = 1; c <= 4; c++) {
+    console.log(c)
+}
+console.log('FIM!')
