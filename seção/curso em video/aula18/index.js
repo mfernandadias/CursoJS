5b78701af8820263effe20854790aa653c2e39dd
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-16 19:43:40 -0300

    exercícios

--- seção/curso em video/aula18/index.js
+++ seção/curso em video/aula18/index.js
@@ -21,8 +21,42 @@ function inLista(n, l) {
 
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
-        
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if(valores.lengtgh == 0) {
+        window.alert('Adione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0 
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos] 
+        }
+        media = soma / tot 
+        res.innerHTML = ''
+        res.innerHTML += `<P>Ao todo, temos ${tot} números cadastrados</p>`
+        res.innerHTML += `<p>O maior valore informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<P>Somando todos os valores, temos ${soma}</p>`
+        res.innerHTML += `<p>A media dos valores digitados é ${media }</p>`
+
+    }
+
 }
