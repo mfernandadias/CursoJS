62ef73d33349140992cafae6f054075de07ce1c2
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-15 23:06:26 -0300

    funções

---
+++ seção/curso em video/aula16/index.js
@@ -0,0 +1,32 @@
+/*function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Impar'
+    }
+}
+
+let res = parimpar(257)
+console.log(res)
+
+let v = function(x) {
+    return x*2
+}
+console.log(v(5))
+//fatorial de um número 
+function fatorial(n) {
+    let fat = 1
+    for(let c = n; c > 1; c--) {
+        fat *= c
+    } 
+    return fat
+}
+
+console.log(fatorial(5)) */
+
+//recursividade
+function fatorial(n) {
+    if (n == 1)
+}
+
+
