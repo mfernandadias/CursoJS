a6b11b5f69caf6ead504c41585392af066086a18
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-17 20:51:50 -0300

    hoisting

---
+++ seção/variaveisDIO/hoisting.js
@@ -0,0 +1,26 @@
+//linguagem procedural
+/*teste();
+
+function teste() {
+    console.log('teste');
+} */
+
+/*var var1; 
+console.log(var1);
+
+var1 = 'teste';
+
+console.log(var1); */
+
+
+var teste1 = function() {
+    console.log('teste1');
+}
+
+function teste2( ){
+    console.log('teste2');
+}
+ 
+teste1();
+teste2();
+
