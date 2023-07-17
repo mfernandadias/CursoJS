30a67e3774125fe4e1cbf1118e8976e926636ff4
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-17 20:41:48 -0300

    variáveis

---
+++ seção/variaveisDIO/variáveis.js
@@ -0,0 +1,13 @@
+//aula de variáveis 
+//javascript é uma linguagem sensitive 
+
+var varNumero = 10;
+var var1 = 10; 
+//var1 = 'testo';
+
+var var2 = '20';
+console.log(var1 - var2);
+
+var varNumero = 10;
+var varTexto = 'Testo';
+console.log(varNumero)
