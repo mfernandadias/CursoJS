422ddb40b087c27f5874c02fcba422b92fc94802
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-17 22:26:59 -0300

    tipos de dados
    wrappers, coerção de tipos, boolean, null e undefined

---
+++ seção/variaveisDIO/2.tipos de dados/tiposdedados.js
@@ -0,0 +1,51 @@
+/* 
+boolean 
+null 
+undefined 
+number 
+string 
+synbol
+*/
+
+// object 
+
+/*let x = 100; 
+x = 'texto';
+
+let y = { numero: 10}
+y.numero = 20 */
+
+/*console.log(10 * '10')
+
+console.log(10 == '10')
+
+console.log(10 === '10') */
+
+//true false 
+
+const nome = ' ';
+
+if (nome) {
+    console.log('preenchido')
+}
+console.log(!!'true')
+
+new Boolean(true);
+
+if ( 10 < 11) {
+    console.log(Boolean)
+}
+
+/*const x = null;
+
+let y;
+
+console.log(x);
+console.log(y); */
+
+//const x = {nome: null};
+
+//object
+const x = {nome: undefined};
+
+console.log(JSON.stringify(x));
