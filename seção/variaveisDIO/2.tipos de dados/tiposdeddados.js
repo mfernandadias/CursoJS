8610cc743540bed750bf13ba57d32ebf7f099dc3
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-17 23:00:45 -0300

    tipos de dados
    numbers, string e symbol

---
+++ seção/variaveisDIO/2.tipos de dados/tiposdeddados.js
@@ -0,0 +1,29 @@
+//tipos de dados, numbers, string e symbol
+
+//const x = { nome: null};
+
+//console.log(JSON.stringify(x));
+
+// +Infinity - infinity e NaN
+//double precision 64-bit binary format IEEE 745
+
+//declaração de string 
+/*"texto"
+'texto'
+`texto`*/
+
+//console.log("<div id='10'> teste </div>")
+
+/*
+const id = 20;
+
+console.log('<div id=" '+ id + '">teste</div>')
+console.log(`div id=${id}></div>`);
+*/
+
+//synbol 
+
+const x = Symbol('10');
+
+console.log(x);
+
