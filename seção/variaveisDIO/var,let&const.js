50b403bc493a0f6b20ddcae34d61441daa491087
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-17 21:04:48 -0300

    diferença de var, let e const

---
+++ seção/variaveisDIO/var,let&const.js
@@ -0,0 +1,38 @@
+
+//var var1; 
+
+/*if (true) {
+    var var1 = 10;
+}
+
+console.log(var1); */
+
+//variáveu var
+/*var var1;
+function teste( ) {
+    var var1 = 10;
+}
+
+teste();
+
+console.log(var1); */
+
+//variável let permanece no bloco, pos isso da erro
+/*if (true ) {
+    let var1 = 10; 
+    console.log(var1);
+}*/
+
+//console.log(var1);
+
+//não pode ter valor alterado 
+/*const x = 10; 
+x = 11; 
+
+console.log(x); */
+
+var teste  = 10; 
+/*
+
+*/
+
