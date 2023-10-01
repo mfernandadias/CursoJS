8cb314a9df18acbf5d4642f626f3dcbe905d1ea4
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-10-01 16:39:31 -0300

    exercício de fixação

--- seção/cursologicadeProgramação/trabalhandocomVariaveis/exerciciodefixaçao.js
+++ seção/cursologicadeProgramação/trabalhandocomVariaveis/exerciciodefixaçao.js
@@ -1,9 +1,15 @@
-//nome, idade, numero de telefone, cpf, endereço 
+//nome, idade, numero de telefone, cpf, endereço, recebe 
 //string = armazenar texto 
 //number = armazenar numero 
 //boolean = armazenar valores lógicos 
 
 let nomePessoa = "Josicreide Do Pythinho"
 let idade = 115
-let numeroTelefone = "11984713388"
+let numeroTelefone = "11-984713388"
+let cpf = "312.122.312-45"
+let endereco = "Rua dos limoeiros, numero 75, bairro "
+let recebePensao = true;
+
+//let somenete na hora de criar 
+idade 
 
