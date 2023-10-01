208245d0b0c01947b286a5c66681c7bd85cd7ca1
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-10-01 16:12:32 -0300

    variaveis

--- seção/cursologicadeProgramação/trabalhandocomVariaveis/const.js
+++ seção/cursologicadeProgramação/trabalhandocomVariaveis/const.js
@@ -1,3 +1,7 @@
-//saída
 
-console.log("Pokemon Go diz:")
+
+//saída
+console.log(notificacao + " tem um novo pekemon na região ")
+console.log(notificacao + " você foi derrotado por um líder")
+
+notificacao = "digi"
