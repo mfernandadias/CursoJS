f7451b7a7154fe6ddb1aef0da5e6f24103ce5d10
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-10-01 18:03:54 -0300

    length = linhas

---
+++ seção/cursologicadeProgramação/vetoresEMatrizes/armarios.js
@@ -0,0 +1,14 @@
+/*let pokemon = ["pikachu", "charmander", "Bulbasaur"]
+pokemon.shift()
+
+console.log(pokemon)*/
+
+let nomesPokemon = ["Pikachu", "Chamander", "Bulbassaur"]
+let timePokemon = [
+    ["pikachu", "M", "level 1"], 
+    ["Chamander", "F", 3]
+]
+
+/*console.log("O pokemon " + timePokemon[1][0] + " é do sexo " + timePokemon[1])*/
+
+console.log(timePokemon.length)
