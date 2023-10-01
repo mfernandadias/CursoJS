be5a7e2ae9d2435196925869b927d9818e8ea539
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-10-01 16:33:30 -0300

    pokemon

---
+++ seção/cursologicadeProgramação/trabalhandocomVariaveis/exerciciodefixaçao.js
@@ -0,0 +1,9 @@
+//nome, idade, numero de telefone, cpf, endereço 
+//string = armazenar texto 
+//number = armazenar numero 
+//boolean = armazenar valores lógicos 
+
+let nomePessoa = "Josicreide Do Pythinho"
+let idade = 115
+let numeroTelefone = "11984713388"
+
---
+++ seção/cursologicadeProgramação/trabalhandocomVariaveis/pokemon.js
@@ -0,0 +1,14 @@
+//pokemon 
+
+let nomePokemon = "pikachu"
+let nivelPokemon = 20
+let pontosDeVidaPokemon = 45 
+let pokemonSexo = ""
+let selecionavel =  true 
+
+nomePokemon = ""
+
+//tipos variáveis 
+/*
+string = 
+*/
