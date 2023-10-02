49abd987ee1a0890d5ee8be126e31ceb4af240eb
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-10-02 17:31:55 -0300

    igual e diferente

---
+++ seção/cursologicadeProgramação/modulo3/operadores/igual&diferente.js
@@ -0,0 +1,26 @@
+
+
+let numero = "1"
+console.log(numero == "1")
+
+// = é atribuição 
+// == é ára comparar o valor 
+// == é para comparar o valor e o formato do conteúdo 
+
+//guarda o valor em uma variável de resultado TRUE ? FALSE 
+let marca = "Apple"
+let resultado = marca === "Samsung"
+
+console.log(resultado)
+
+let cpfBloqueado = "123.451.145-45"
+let cpfUsuario = "222.111.222-09"
+let ehCPFBloqueado = cpfBloqueado === cpfUsuario
+
+console.log("O usuario pode embarcar ? " + ehCPFBloqueado)
+
+let idadeMinima = 18
+let idadeUsuario = 17
+let idadePermitidaValida = idadeusuario > idadeMinima 
+
+console.log(idadePermitidaValida)
