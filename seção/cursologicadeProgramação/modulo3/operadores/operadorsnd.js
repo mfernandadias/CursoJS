1656a4238a484e9377adfd361fa98e4ddfd07085
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-10-02 18:26:52 -0300

    operador AND

---
+++ seção/cursologicadeProgramação/modulo3/operadores/operadorsnd.js
@@ -0,0 +1,13 @@
+// AND (&&) 
+
+/*let idade = 17
+let vistoVerif = true 
+console.log((idade >= 18) && (vistoVerificado === true))
+console.log(resultado)*/
+
+//AND (&&) - 100 moedas coletadas E 1 item estrela 
+let moedasColetadas = 99
+let item = "estrela"
+let resultado = (moedasColetadas >=  100 ) && (item === "estrela")
+console.log(resultado)
+
