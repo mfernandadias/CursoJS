22544d45133ea75438bc1aa4c37165bde0eaf0af
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-10-02 20:22:35 -0300

    if else

---
+++ seção/cursologicadeProgramação/modulo3/operadores/if/ifelse.js
@@ -0,0 +1,11 @@
+let possuiOvos = true 
+let itensComprados = " "
+
+if(possuiOvos) {
+    itensComprados = "Leite"
+}
+else {
+    console.log("Possou na sessão de congelados")
+} itensComprados = "Lasanha congelada"
+
+console.log("item comprado: " + itensComprados)
