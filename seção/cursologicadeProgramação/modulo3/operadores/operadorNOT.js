689004752807369a6addc31459e113c6af91a789
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-10-02 18:41:12 -0300

    operador NOT

---
+++ seção/cursologicadeProgramação/modulo3/operadores/operadorNOT.js
@@ -0,0 +1,9 @@
+/*let tempo = "chuva"
+let resultado = tempo !== "chuva"
+console.log(!!resultado)*/
+
+// NOT (!) - nega uma afirmação ]
+let empo = "chuva"
+let horario = 8
+let resultado = !((tempo !== "chuva") && (horario > 6))
+console.log(resultado)
