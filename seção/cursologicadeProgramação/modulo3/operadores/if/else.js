743def430d9e32b2eb4baa94aeae3d2be018c710
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-10-02 20:13:45 -0300

    if

---
+++ seção/cursologicadeProgramação/modulo3/operadores/if/else.js
@@ -0,0 +1,10 @@
+//let ehLigado = true 
+
+let possuiOvos = false 
+let itensComprados = "Leite"
+
+if(possuiOvos) {
+    itensComprados = "Leite"
+}
+
+console.log("Item comprado: " + itensComprados )
