d83f48b4cb939f4a4a28abb5d418410d76a3acf3
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-10-03 18:30:19 -0300

    function 02

---
+++ seção/cursologicadeProgramação/funçao/function02.js
@@ -0,0 +1,25 @@
+//função que pega todos os dados 
+mainSaveData()
+
+function mainSaveData(){
+    getData()
+    checkValues()
+    sendToDataBase()
+}
+
+function getData() {
+    console.log("pegando dados do usuário")
+    if (1 < 3) {
+        console.log("numero encontrado")
+        console.log("numero encontrado")
+        console.log("numero encontrado")
+    }
+}
+
+function checkValues(){
+    console.log("validando dados")
+}
+
+function sendToDatabase() {
+    console.log("cada")
+}
