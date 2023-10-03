31269f51cec7a020ef6b7cf82984282ad1ae9ae4
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-10-03 17:18:32 -0300

    switch

--- seção/cursologicadeProgramação/modulo3/operadores/if/swtch.js
+++ seção/cursologicadeProgramação/modulo3/operadores/if/swtch.js
@@ -1,18 +1,20 @@
 // estrutura de decisão 
 //switch/case/default 
 
-let fruta = "pera"
+let fruta = "1"
 
 switch (fruta) {
     case "laranja":
     console.log("suco de laranja")
     break
 
-    case "banana":
+    case "1":
     console.log("vitamina de banana")
+    console.log("segunda mensagem")
     break
 
     case "maça":
+    case "morango":
     console.log("suco de maça")
     break
 
