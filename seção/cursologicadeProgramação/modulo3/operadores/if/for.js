93eb13c224908bc0dc06445b7a5715796805b2c0
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-10-03 17:30:24 -0300

    for

--- seção/cursologicadeProgramação/modulo3/operadores/if/for.js
+++ seção/cursologicadeProgramação/modulo3/operadores/if/for.js
@@ -1,6 +1,18 @@
 //for 
 
-for (let contador = 0; contador < 4; contador++) {
+/*for (let contador = 0; contador < 4; contador++) {
     console.log(contador)
     console.log("aumentar o contador")
+} */
+
+//for 
+
+let pontosDeVida = 0
+
+for (let i = 0; i < 11; i++) {
+    pontosDeVida = 1
+    //pontosDeVida = Pontos
+    console.log("Tomou poção mágica " + i)
 }
+
+console.log(pontosDeVida + " Totais ")
