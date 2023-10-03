c0950e1dece17743f2b9e2fa625a78657b3961c2
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-10-03 17:03:43 -0300

    break

---
+++ seção/cursologicadeProgramação/modulo3/operadores/if/swtch.js
@@ -0,0 +1,18 @@
+// estrutura de decisão 
+//switch/case/default 
+
+let fruta = "banana"
+
+switch (fruta) {
+    case "laranja":
+    console.log("suco de laranja")
+    break
+
+    case "banana":
+    console.log("vitamina de banana")
+    break
+
+    case "maça":
+    console.log("suco de maça")
+    break
+}
