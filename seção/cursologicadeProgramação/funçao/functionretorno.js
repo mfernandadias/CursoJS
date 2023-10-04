76e9004ac2e7e8ebc680e2c4296e69e9200faa08
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-10-04 16:19:34 -0300

    retorno de function

---
+++ seção/cursologicadeProgramação/funçao/functionretorno.js
@@ -0,0 +1,17 @@
+/*let resultado = soma(5, 10)
+
+console.log("O resultado dessa função" + resultado)
+
+function soma(numA, numB) {
+    let somatorio = numA + numB
+    return somatorio
+}*/
+
+let resultado = soma(5, 10)
+
+console.log("O resultado dessa função é " + resultado)
+
+function soma(numA, numB) {
+    let somatorio = [3, 11, 90]
+    return somatorio
+}
