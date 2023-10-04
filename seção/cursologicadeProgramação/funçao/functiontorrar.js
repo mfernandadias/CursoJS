2a16c78b4df6ceb109617203e5ddf99d8bf68623
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-10-03 22:36:41 -0300

    function torrar

--- seção/cursologicadeProgramação/funçao/functiontorrar.js
+++ seção/cursologicadeProgramação/funçao/functiontorrar.js
@@ -7,11 +7,17 @@ function torrar(pao, nome) {
     console.log("Torrada feita com " + pao)
     console.log(nome)
 }*/
-
-torrar ("pão na chapa")
-
+/*torrar("pão na chapa", undefined, "felipe")
+//torrar ("pão na chapa")
+//não é uma b
 function torrar(pao, nome = "Cliente", valor=99.90) {
     console.log("torrada feita com " + pao)
     console.log("ele é um pedido de " + nome)
     console.log("O valor total é " + valor)
+} */
+
+function createStringConnection(databasename) {
+    console.log("connect:DBCONNECT; user=felipe; pass=1234; initial_d atabase= " + databasename)
 }
+
+createStringConnection("db_products")
