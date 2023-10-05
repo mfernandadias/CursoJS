235130e78727c58ef3cd73e75b380a62c8fe7dcd
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2023-10-05 16:11:22 -0300

    json

--- seção/cursologicadeProgramação/JSON/json.js
+++ seção/cursologicadeProgramação/JSON/json.js
@@ -1,7 +1,39 @@
 //JSON - JavaScript Object Notation 
 //chave e valor com o bjetivo de transferir dados 
 
-let name = "Felipe"
+/*let invoide = {
+    name: "felipe",
+    age: 28, 
+        product0{
+            0: ["mouse 2xwm", 29.90], 
+            1: ["Teclado mecânico", 129.90], 
+            2: ["Monitor", "899;90"]
+        }, 
+        texas: "98.90"
+}
+
+generateInvoice(invoice)
+
+function generateInvoice(invoiceProducts){
+    console.log(`O comprador é ${invoice.name}`)
+    console.log(`A idade é ${invoice.age}`)
+    console.log("--------------------")
+    console.log(`Os produtos são ${invoice.products}`)
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*let name = "Felipe"
 let age = 28
 let products = ["mouse 2xwm", "teclado mecânico", "Monitor"]
 let productsValues = [29.90, 129.99, 899.99]
@@ -13,4 +45,4 @@ function generateInvoice(name){
     console.log("-------------------")
     console.log("O comprador é " + products[0])
     console.log("O valor é " + productsValues[0])
-}
+}*/
